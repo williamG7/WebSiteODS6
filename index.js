2559525ebf616b529de223fadcd62b75db3b52f2
ViktoriiaPonomarenko <viktoriia.ponomarenko.7e6@itb.cat>
2025-05-28 12:28:11 +0200

    Primer commit

---
+++ index.js
@@ -0,0 +1,95 @@
+
+import express from "express";
+import fs from "fs";
+import bodyParser from "body-parser";
+
+const app = express();
+app.use(bodyParser.json());
+
+/*------------------vertidos------------------------*/
+
+const readVertidos = () => {
+    try {
+        const data = fs.readFileSync("./jsons_folders/Vertidos.json");
+        return JSON.parse(data);
+    } catch (error) {
+        console.error(error);
+    }
+};
+
+app.get("/vertidos",(req,res)=>{
+    const data=readVertidos();
+    res.json(data);
+});
+
+/*-------------------Residus_recollida------------------------*/
+
+const readResidus_recollida = () => {
+    try {
+        const data = fs.readFileSync("./jsons_folders/Residus_recollida.json");
+        return JSON.parse(data);
+    } catch (error) {
+        console.error(error);
+    }
+};
+
+app.get("/residusRecollida",(req,res)=>{
+    const data=readResidus_recollida();
+    res.json(data);
+});
+
+/*-------------------Estadistiques------------------------*/
+
+
+const readEstadistiques = () => {
+    try {
+        const data = fs.readFileSync("./jsons_folders/Estadistiques.json");
+        return JSON.parse(data);
+    } catch (error) {
+        console.error(error);
+    }
+};
+
+app.get("/estadistiques",(req,res)=>{
+    const data=readEstadistiques();
+    res.json(data);
+});
+/*-----------------------EstacionsRegeneracio----------------------------*/
+
+const readEstacionsRegeneracio = () => {
+    try {
+        const data = fs.readFileSync("./jsons_folders/EstacionsRegeneracio.json");
+        return JSON.parse(data);
+    } catch (error) {
+        console.error(error);
+    }
+};
+
+app.get("/estacionsRegeneracio",(req,res)=>{
+    const data=readEstacionsRegeneracio();
+    res.json(data);
+});
+
+/*----------------------Embalses-----------------------------*/
+
+const readEmbalses = () => {
+    try {
+        const data = fs.readFileSync("./jsons_folders/Embalses.json");
+        return JSON.parse(data);
+    } catch (error) {
+        console.error(error);
+    }
+};
+
+app.get("/embalses",(req,res)=>{
+    const data=readEmbalses();
+    res.json(data);
+});
+
+
+
+
+//Funció per escoltar
+app.listen(3000, ()=>{
+    console.log("Server listing on port 3000");
+});
